9d241ac424c5a2c720c9d98fb5463f5555fe0151
unknown <C:\Users\\AppData\Roaming\The Bat!>
2025-05-15 19:43:20 +0300

    ustal

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,9 @@
+import 'dotenv/config'
 import { Telegraf } from 'telegraf'
 import navigationHandler from './handlers/navigationHandler.js'
 import mainMenu from './keyboard/mainMenu.js'
 
-const bot = new Telegraf(BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start(ctx => {
 	ctx.reply('Добро пожаловать! Выберите раздел:', mainMenu())
